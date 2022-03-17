7fac00e573fe05e81739491bd3128a57222926a5
Sunal Jabili <sunaljabili@Sunals-MacBook-Pro.local>
2022-03-18 02:10:52 +0400

    task completed

---
+++ 17Classworks/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace _17Classworks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(letterCount("alma",'a'));
+
+            //Console.WriteLine(reversWord("12343"));
+        }
+
+        #region Task-1
+
+        //static int letterCount(string word,char letter)
+        //{
+        //    int count = 0;
+        //    for (int i = 0; i < word.Length; i++)
+        //    {
+
+        //        if (word[i] == letter)
+        //        {
+        //            count++;
+        //        }
+        //    }
+        //    return count;
+        //}
+        #endregion
+
+        #region Task-2
+
+        //static string reversWord(string word)
+        //{
+        //    string reverse = "";
+        //    for (int i = word.Length - 1; i >= 0; i--)
+        //    {
+        //        reverse += word[i];
+        //    }
+        //    return reverse;
+        //}
+
+
+        #endregion
+
+    }
+}
